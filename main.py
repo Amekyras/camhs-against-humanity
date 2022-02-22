a1f88eafa08b3b486cf4388cfe6625028770072a
amekyras <ash.skeet@pobox.com>
2022-02-22 21:40:46 +0000

    empty
    Signed-off-by: amekyras <ash.skeet@pobox.com>

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# import openpyxl
+import openpyxl
+
 
 class Card:
     def __init__(self, card_colour, card_id, card_options, card_text):
@@ -22,6 +23,25 @@ class Player:
         self.score = 0
 
 
+# Card loader and settings
+white_card_count = 188
+black_card_count = 103
+white_card_column = 1
+black_card_column = 2
+option_column = 3
+start_row = 2
+white_cards = []
+black_cards = []
+
+card_lib = openpyxl.load_workbook("data.xlsx")
+card_sheet = card_lib.active
+
+for i in range(0, white_card_count):
+    white_cards.append(Card("White", i, 0, card_sheet.cell(row=(i+start_row), column=white_card_column).value))
+
+for i in range(0, black_card_count):
+    black_cards.append(Card("Black", i, card_sheet.cell(row=i+start_row, column=option_column).value,
+                            card_sheet.cell(row=i+start_row, column=black_card_column).value))
 # Game variables
 player_num = 5
 hand_size = 10
