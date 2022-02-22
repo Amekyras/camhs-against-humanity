47bfdb3494c0d15d34455252a26763685713e8eb
amekyras <ash.skeet@pobox.com>
2022-02-22 18:35:15 +0000

    empty

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-# Game variables
-player_num = 5
-hand_size = 10
-# TODO make gamevars input()
-# TODO write Player class
-class Player:
-    def __init__(self, player_is_human, player_score, player_is_czar, player_name):
-        self.human = player_is_human
-        self.score = player_score
-        self.czar = player_is_czar
-        self.name = player_name
+import openpyxl
 
-# TODO write function to populate classes for each card
-# TODO write Card class
 class Card:
     def __init__(self, card_colour, card_id, card_options, card_text):
         self.colour = card_colour
@@ -20,5 +8,27 @@ class Card:
         self.text = card_text
 
 
+class Player:
+    def __init__(self, player_is_human, player_score, player_is_czar, player_name):
+        self.human = player_is_human
+        self.score = player_score
+        self.czar = player_is_czar
+        self.name = player_name
+
+    def add_score(self):
+        self.score += 1
+
+    def reset_score(self):
+        self.score = 0
+
+
+# Game variables
+player_num = 5
+hand_size = 10
+
+# TODO make gamevars input()
+# TODO write Player classes
+# TODO write function to populate classes for each card
+# TODO write Card class
 # TODO write main game loop
 # TODO write AI players
