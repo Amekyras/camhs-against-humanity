1e9baf319951fc4eb6c2f3161a1aba1573a9952a
amekyras <ash.skeet@pobox.com>
2022-02-23 11:58:17 +0000

    given players cards
    Signed-off-by: amekyras <ash.skeet@pobox.com>

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import openpyxl
+import random
+import os
+
+
+def clearscreen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 class Card:
@@ -10,11 +16,13 @@ class Card:
 
 
 class Player:
-    def __init__(self, player_is_human, player_score, player_is_czar, player_name):
+    def __init__(self, player_id, player_is_human, player_score, player_is_czar, player_name):
+        self.id = player_id
         self.human = player_is_human
         self.score = player_score
         self.czar = player_is_czar
         self.name = player_name
+        self.hand = []
 
     def add_score(self):
         self.score += 1
@@ -22,6 +30,8 @@ class Player:
     def reset_score(self):
         self.score = 0
 
+    def add_card(self):
+        self.hand.append(white_cards.pop())
 
 # Card loader and settings
 white_card_count = 188
@@ -37,13 +47,13 @@ card_lib = openpyxl.load_workbook("data.xlsx")
 card_sheet = card_lib.active
 
 for i in range(0, white_card_count):
-    white_cards.append(Card("White", i, 0, card_sheet.cell(row=(i+start_row), column=white_card_column).value))
+    white_cards.append(Card("White", str(i)+"W", 0, card_sheet.cell(row=(i+start_row), column=white_card_column).value))
 
 for i in range(0, black_card_count):
-    black_cards.append(Card("Black", i, card_sheet.cell(row=i+start_row, column=option_column).value,
+    black_cards.append(Card("Black", str(i)+"B", card_sheet.cell(row=i+start_row, column=option_column).value,
                             card_sheet.cell(row=i+start_row, column=black_card_column).value))
 # Game variables
-player_num = 5
+player_num = 1
 hand_size = 10
 players = []
 
@@ -54,7 +64,15 @@ players = []
 # TODO write AI players
 
 # Player setup
-for i in range (1, player_num):
-    players[i] = Player(True, 0, False, input("What is your name?"))
-
+for i in range(0, player_num):
+    players.append(Player(i, True, 0, False, input("What is your name?")))
+clearscreen()
 # Main game loop
+# Give players cards
+random.shuffle(white_cards)
+for i in range(0, len(players)):
+    while len(players[i].hand) < hand_size:
+        players[i].add_card()
+for i in range(0, 10):
+    print(players[0].hand[i].text)
+# check if Czar exists
